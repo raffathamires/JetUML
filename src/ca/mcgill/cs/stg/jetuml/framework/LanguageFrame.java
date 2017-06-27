2751b96dc56db3ce7f3341d5f130b785c7f617ac
Joacir Junior <contato@joacirjunior.com>
2017-06-27 03:44:19 -0300

    #26

--- src/ca/mcgill/cs/stg/jetuml/framework/LanguageFrame.java
+++ src/ca/mcgill/cs/stg/jetuml/framework/LanguageFrame.java
@@ -23,63 +23,20 @@ package ca.mcgill.cs.stg.jetuml.framework;
 
 import java.util.Locale;
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.Image;
-import java.awt.Label;
 import java.awt.Toolkit;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.beans.PropertyVetoException;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.prefs.Preferences;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextArea;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.MenuEvent;
-import javax.swing.event.MenuListener;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 import ca.mcgill.cs.stg.jetuml.UMLEditor;
 import ca.mcgill.cs.stg.jetuml.diagrams.ClassDiagramGraph;
@@ -87,10 +44,7 @@ import ca.mcgill.cs.stg.jetuml.diagrams.ObjectDiagramGraph;
 import ca.mcgill.cs.stg.jetuml.diagrams.SequenceDiagramGraph;
 import ca.mcgill.cs.stg.jetuml.diagrams.StateDiagramGraph;
 import ca.mcgill.cs.stg.jetuml.diagrams.UseCaseDiagramGraph;
-import ca.mcgill.cs.stg.jetuml.graph.Edge;
-import ca.mcgill.cs.stg.jetuml.graph.Graph;
-import ca.mcgill.cs.stg.jetuml.graph.GraphElement;
-import ca.mcgill.cs.stg.jetuml.graph.Node;
+
 
 /**
  * Frame de seleção do idioma
@@ -102,28 +56,10 @@ public class LanguageFrame extends JFrame {
 	
 	private Locale locale;	
 
-	private static final int FRAME_GAP = 20;
-	private static final int ESTIMATED_FRAMES = 5;
-	private static final int MAX_RECENT_FILES = 8;
 	private static final int MARGIN_SCREEN = 8; // Fraction of the screen to leave around the sides
-	private static final int MARGIN_IMAGE = 2; // Number of pixels to leave around the graph when exporting it as an image
-	private static final int HELP_MENU_TEXT_WIDTH = 10; //Number of pixels to give to the width of the  text area of the Help Menu.
-	private static final int HELP_MENU_TEXT_HEIGHT = 40; //Number of pixels to give to the height of the text area of the Help Menu.
-		
+
 	private ResourceBundle aLanguagesResources;
-	
-	private MenuFactory aAppFactory;
-	private ResourceBundle aAppResources;
-	private ResourceBundle aVersionResources;
-	private ResourceBundle aEditorResources;
-	private JTabbedPane aTabbedPane;
-	private ArrayList<JInternalFrame> aTabs = new ArrayList<>();
-	private JMenu aNewMenu;
-	private Clipboard aClipboard = new Clipboard();
-	
-	private RecentFilesQueue aRecentFiles = new RecentFilesQueue();
-	private JMenu aRecentFilesMenu;
-	
+
 	public LanguageFrame(){
 	 	this.init();
 	}
